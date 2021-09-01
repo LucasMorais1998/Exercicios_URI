490467c9575a9109fc3c6510cdad38d06a4062c4
Lucas M <lucas.msantos68@senacsp.edu.br>
2021-09-01 19:27:00 -0300

    Corrigindo um erro do 1180

---
+++ 1180.Menor_e_Posição/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1180.Menor_e_Posição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] N = new int[10];
+
+            int Menor = ;
+            int Posicao = 0;
+
+            for (int i = 0; i < N.Length; i++)
+            {
+                N[i] = int.Parse(Console.ReadLine());
+
+                if (Menor > N[i])
+                {
+                    Menor = N[i];
+                    Posicao = i;
+                }
+            }
+            
+            Console.WriteLine("Menor valor: " + Menor);
+            Console.WriteLine("Posicao: " + Posicao);
+            
+
+
+           
+
+        }
+    }
+}
