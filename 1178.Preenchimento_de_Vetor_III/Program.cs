93bb38c7a4bd47d2fc4b43285c80e90be100e61e
Lucas M <lucas.msantos68@senacsp.edu.br>
2021-09-01 17:15:27 -0300

    Exercicios URI

--- 1178.Preenchimento_de_Vetor_III/Program.cs
+++ 1178.Preenchimento_de_Vetor_III/Program.cs
@@ -7,16 +7,29 @@ namespace _1178.Preenchimento_de_Vetor_III
     {
         static void Main(string[] args)
         {
-            double[] N = new double[100];
+            decimal[] N = new decimal[100];
 
-            double X = double.Parse(Console.ReadLine());
+            decimal Aux = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            N[0] = X;
+            N[0] = Aux;
 
-            for (int i = 1; i < N.Length; i++)
+
+            //for (int i = 0 + 1; i < N.Length; i++)
+            //{
+
+            //    N[i] = N[i - 1] / 2.0;
+
+            //}
+
+            for (int i = 1; i < 100; i++)
             {
-                N[i] = N[i - 1] / 2.0;
-                Console.WriteLine(N[i].ToString("F4", CultureInfo.InvariantCulture));
+                Aux = (Aux / 2);
+                N[i] = Aux;
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("N[" + i + "] = " + N[i].ToString("F4", CultureInfo.InvariantCulture));
             }
 
         }
