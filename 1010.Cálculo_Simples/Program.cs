06409e623d07dcfb719cddf34d44c78ca15ecea7
Lucas M <lucas.msantos68@senacsp.edu.br>
2021-09-02 01:22:59 -0300

    Algumas correções

--- 1010.Cálculo_Simples/Program.cs
+++ 1010.Cálculo_Simples/Program.cs
@@ -7,15 +7,23 @@ namespace _1010.Cálculo_Simples
     {
         static void Main(string[] args)
         {
-            double r = double.Parse(Console.ReadLine());
-            r = Math.Pow(r, 3);
+            string[] peca1 = Console.ReadLine().Split(' ');
 
-            double pi = 3.14159;
-            double volume = ((4 / 3.0) * pi) * r;
+            int codPeca1 = int.Parse(peca1[0]);
+            int numPeca1 = int.Parse(peca1[1]);
+            double valPeca1 = Convert.ToDouble(peca1[2], CultureInfo.InvariantCulture);
 
-            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+            double totPeca1 = numPeca1 * valPeca1;
 
+            string[] peca2 = Console.ReadLine().Split(' ');
 
+            int codPeca2 = int.Parse(peca2[0]);
+            int numPeca2 = int.Parse(peca2[1]);
+            double valPeca2 = Convert.ToDouble(peca2[2], CultureInfo.InvariantCulture);
+
+            double totPeca2 = numPeca2 * valPeca2;
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + (totPeca1 + totPeca2).ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
