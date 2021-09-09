71cf4859cb3f8fe895d9cab16cdac721f2c83804
Lucas M <lucas.msantos68@senacsp.edu.br>
2021-09-09 19:37:07 -0300

    Exercicios URI

---
+++ 1759.Ho_Ho_Ho/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1759.Ho_Ho_Ho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N - 1; i++)
+            {
+                Console.Write("Ho ");
+            }
+
+            Console.Write("Ho!\n");
+
+        }
+    }
+}
---
+++ 1930.Tomadas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1930.Tomadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Entrada = Console.ReadLine().Split(' ');
+            int[] T = new int[4];
+            int Result = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                T[i] = int.Parse(Entrada[i]);
+                int Bocas = T[i];
+                Result += Bocas;
+            }
+            Console.WriteLine(Result - (T.Length - 1));
+        }
+    }
+}
