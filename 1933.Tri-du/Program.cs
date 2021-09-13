7457f14504a6bd684884b6a1742aae90399ee6c9
Lucas M <lucas.msantos68@senacsp.edu.br>
2021-09-13 19:42:09 -0300

    Exercicios URI

---
+++ 1933.Tri-du/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _1933.Tri_du
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Entrada = Console.ReadLine().Split(' ');
+          
+            int A = int.Parse(Entrada[0]);
+            int B = int.Parse(Entrada[1]);
+
+            int primeira = 0, segunda = 0, terceira = 0;
+
+            if (A >= 1 && A <= 13)
+            {
+                primeira = A;             
+            }
+
+            if (B >= 1 && A <= 13)
+            {
+                segunda = B;
+            }
+
+            if (primeira > segunda)
+            {
+                terceira = primeira;
+            }
+            else
+            {
+                if (segunda > primeira)
+                {
+                    terceira = segunda;
+                }
+                else
+                {
+                    terceira = primeira;
+                }
+            }
+            Console.WriteLine(terceira);
+        }
+    }
+}
